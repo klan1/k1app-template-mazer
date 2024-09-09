28f282b3ea70b6d230f6c3476896c3f7c5f7f20c
j0hnd03 <alejo@klan1.com>
2024-09-09 13:43:38 -0500

    better constants

--- src/init.php
+++ src/init.php
@@ -25,12 +25,4 @@ use const k1app\K1APP_BASE_URL;
 
 const VERSION = "0.1";
 const TPL_PATH = __DIR__;
-define('k1app\template\mazer\TPL_URL', K1APP_BASE_URL . strstr(dirname(__DIR__, 1), 'vendor') . '/dist/');
-
-// Peace for user, info for the developer with ZendZerver and Z-Ray Live!
-error_reporting(E_ALL & ~E_NOTICE & ~E_USER_NOTICE);
-
-/*
- * PATH AUTO CONFIG
- */
-define("K1APP_TEMPLATE_BASE_PATH", dirname(__FILE__));
+// define('k1app\template\mazer\TPL_URL', K1APP_BASE_URL . strstr(dirname(__DIR__, 1), 'vendor') . '/dist/');
