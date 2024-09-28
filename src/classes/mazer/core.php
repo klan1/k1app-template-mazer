80e0dc5f162fd1e963643a98f794e0f1cc63a266
j0hnd03 <alejo@klan1.com>
2024-09-28 10:29:12 -0500

    dev progress

--- src/classes/mazer/core.php
+++ src/classes/mazer/core.php
@@ -3,7 +3,7 @@ namespace k1app\template\mazer;
 
 use const k1app\K1APP_BASE_URL;
 
-const VERSION = "0.3";
+const VERSION = "0.4";
 const TPL_PATH = __DIR__;
 define('k1app\template\mazer\TPL_URL', K1APP_BASE_URL . strstr(dirname(__DIR__, 3), 'vendor') . '/dist/');
 
