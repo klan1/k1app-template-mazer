ba31c87949d5fad6ef93fa6d23c2e75addd3547d
j0hnd03 <alejo@klan1.com>
2024-09-28 10:29:12 -0500

    dev progress

--- src/classes/mazer/layouts/single_page.php
+++ src/classes/mazer/layouts/single_page.php
@@ -7,11 +7,13 @@
 namespace k1app\template\mazer\layouts;
 
 use k1app\template\mazer\core;
+use k1app\template\mazer\pages\standard as standar_page;
 use k1app\template\mazer\redefinitions\body;
 use k1app\template\mazer\redefinitions\head;
 
+class single_page extends core {
 
-class blank extends core {
+    protected standar_page $page;
 
     function __construct($lang = 'en', $generate_app_div = false) {
         parent::__construct($lang, true, true);
@@ -21,6 +23,9 @@ class blank extends core {
 
         $this->body = new body($generate_app_div);
         $this->append_child($this->body);
+
+        $content_div = $this->body->append_div('container mt-3');
+        $this->page = new standar_page($content_div);
     }
 
     function head(): head {
@@ -29,5 +34,13 @@ class blank extends core {
 
     function body(): body {
         return $this->body;
+    }
+
+    /**
+     * 
+     * @return standar_page
+     */
+    function page(): standar_page {
+        return $this->page;
     }
 }
